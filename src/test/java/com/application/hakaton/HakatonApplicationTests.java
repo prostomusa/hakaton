a323d9ad30946b286ffd2f82d85bd2c0e2f2c7a4
Мага <zana@MacBook-Air.local>
2023-10-09 00:13:54 +0300

    Initial

---
+++ src/test/java/com/application/hakaton/HakatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.hakaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
