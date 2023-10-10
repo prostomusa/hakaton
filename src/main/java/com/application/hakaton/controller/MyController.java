b64e2a744e4a6bd13479e3c26a2028c57d0c3a97
prostomusa <abdurahmanov.musa@mail.ru>
2023-10-11 00:14:23 +0300

    Initial commit

---
+++ src/main/java/com/application/hakaton/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.application.hakaton.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String welcomeAsHTML() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+    }
+}
